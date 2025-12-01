3cc4eeeb7f6ec7192d47fc3d8ba03c18880db7a8
DIVAKAR <rdivakar0110@gmail.com>
2025-12-01 17:42:42 +0530

    Use DATABASE_URL for Postgres on Render

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -56,12 +56,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        env="DATABASE_URL",  # read from env var
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
